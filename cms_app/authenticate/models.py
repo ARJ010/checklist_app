3074177fa96f545399c719dc0d5f46c89b466561
Abhinav Raj <129362857+ARJ010@users.noreply.github.com>
2024-02-09 10:37:52 +0530

    .

--- cms_app/authenticate/models.py
+++ cms_app/authenticate/models.py
@@ -1,3 +1,34 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+
+class Employee(AbstractUser):
+    employee_id = models.IntegerField(unique=True)
+
+    EMPLOYEE_TYPES = [
+        ('admin', 'Admin'),
+        ('user', 'User'),
+        ('checker', 'Checker'),
+    ]
+    employee_type = models.CharField(max_length=10, choices=EMPLOYEE_TYPES)
+
+    # Specify custom related_name for groups and user_permissions fields
+    groups = models.ManyToManyField(
+        'auth.Group',
+        verbose_name='groups',
+        blank=True,
+        related_name='employees_groups',
+        related_query_name='employee_group',
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        verbose_name='user permissions',
+        blank=True,
+        related_name='employees_permissions',
+        related_query_name='employee_permission',
+    )
+
+    def __str__(self):
+        return self.username
