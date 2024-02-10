2ff282f1ec2e7ec9961682af7eb58c4a4214b3ad
Abhinav Raj <129362857+ARJ010@users.noreply.github.com>
2024-02-10 12:02:08 +0530

    added user search

--- cms_app/manager/views.py
+++ cms_app/manager/views.py
@@ -38,19 +38,23 @@ def register_user(request):
             gender = form.cleaned_data['gender']
 
             # Create User
-            user = User.objects.create_user(username=username, email=email, password=password, first_name=first_name, last_name=last_name)
+            user = User.objects.create_user(
+                username=username, email=email, password=password, first_name=first_name, last_name=last_name)
             if user_type == 'user':
                 user.groups.add(Group.objects.get(name='Users'))
             elif user_type == 'checker':
                 user.groups.add(Group.objects.get(name='Checkers'))
 
             # Create Employee
-            employee = Employee.objects.create(user=user, first_name=first_name, last_name=last_name, user_photo=user_photo, age=age, gender=gender)
+            employee = Employee.objects.create(
+                user=user, first_name=first_name, last_name=last_name, user_photo=user_photo, age=age, gender=gender)
             employee.save()
 
-            return redirect('login')  # Redirect to login page after registration
+            # Redirect to login page after registration
+            return redirect('login')
 
     return render(request, 'manager/register.html', {'form': form})
+
 
 @login_required
 @user_passes_test(employee_group_required)
@@ -60,18 +64,25 @@ def all_users(request):
         users_group = Group.objects.get(name='Users')
         all_users = Employee.objects.filter(user__groups=users_group)
         return render(request, 'manager/all_users.html', {'all_users': all_users})
-        
+
     elif user_type == 'checker':
         users_group = Group.objects.get(name='Checkers')
         all_users = Employee.objects.filter(user__groups=users_group)
         return render(request, 'manager/all_users.html', {'all_users': all_users})
-    
 
-def delete_users(request):
-    if request.method == 'POST':
-        user_ids = request.POST.getlist('user_ids')
-        User.objects.filter(id__in=user_ids).delete()
-        return redirect('all_users')
-    else:
-        # Handle GET request if needed
-        pass
+
+def search_users(request):
+    user_type = request.GET.get('user_type')
+    if user_type == 'user':
+        query = request.GET.get('q')
+        users_group = Group.objects.get(name='Users')
+        all_users = Employee.objects.filter(first_name__icontains=query, user__groups=users_group) | Employee.objects.filter(
+            last_name__icontains=query, user__groups=users_group)
+        return render(request, 'manager/all_users.html', {'all_users': all_users, 'query': query})
+
+    elif user_type == 'checker':
+        query = request.GET.get('q')
+        users_group = Group.objects.get(name='Checkers')
+        all_users = Employee.objects.filter(first_name__icontains=query, user__groups=users_group) | Employee.objects.filter(
+            last_name__icontains=query, user__groups=users_group)
+        return render(request, 'manager/all_users.html', {'all_users': all_users, 'query': query})
